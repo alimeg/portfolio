3eb3ce5d815c2727c9b91a43328f6e0c15e4d3a5
Ali Meghalet <ameghalet@hotmail.com>
2025-01-03 19:23:32 -0500

    Hidded Footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -16,6 +16,12 @@ function ArrowIcon() {
 }
 
 export default function Footer() {
+  const showFooter = false; 
+
+  if (!showFooter) {
+    return null; // no footer to be displayed till showFooter is true
+  }
+
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
@@ -57,5 +63,6 @@ export default function Footer() {
         © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
-  )
+  );
 }
+
